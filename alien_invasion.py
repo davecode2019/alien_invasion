509c3d8b04ee6b1225bc90e4170794a315e88466
davecode2019 <dave.code2019@gmail.com>
2022-08-08 16:11:09 +0100

    Added the alien class and drew a single alien near the top left of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -16,15 +17,19 @@ def run_game():
 
     # Make a ship.
     ship = Ship(screen, ai_settings)
+    
     # Make a group to store bullets in.
     bullets = Group()
+
+    # Make an alien.
+    alien = Alien(ai_settings, screen)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
